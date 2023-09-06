7385a123ebabb7a24dbb8f883ea9cad3f2256cb1
Taejin Kim <rlaxowls1316@naver.com>
2023-09-06 13:46:39 +0900

    :lipstick: 홈 공연 리스트 레이아웃

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -8,9 +8,38 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <Wrapper>
-      <Contents></Contents>
+      <Contents>
+        <div>
+          <L_row>
+            <L_col>
+              <div style={{ backgroundColor: "orange" }}>...</div>
+            </L_col>
+            <L_col>
+              <div style={{ backgroundColor: "orange" }}>...</div>
+            </L_col>
+            <L_col>
+              <div style={{ backgroundColor: "orange" }}>...</div>
+            </L_col>
+            <L_col>
+              <div style={{ backgroundColor: "orange" }}>...</div>
+            </L_col>
+          </L_row>
+        </div>
+      </Contents>
     </Wrapper>
   );
 };
+
+const L_row = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 -5px;
+  row-gap: 10px;
+`;
+
+const L_col = styled.li`
+  width: 33.33333%;
+  padding: 0 5px;
+`;
 
 export default Home;
