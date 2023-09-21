8c0703af494e4854661b2abcdcffa1151df34696
Taejin Kim <rlaxowls1316@naver.com>
2023-09-21 11:00:10 +0900

    :sparkles: 네이버 로그인 기능 추가

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -5,28 +5,23 @@ import { SiNaver } from "react-icons/si";
 import { useEffect } from "react";
 
 export default function Login() {
-  useEffect(() => {
-    // var naver_id_login = new naver_id_login(
-    //   "J71XESavo8zEzLQDSu3x",
-    //   "http://localhost:5500/"
-    // );
-    // var state = naver_id_login.getUniqState();
-    // naver_id_login.setButton("white", 2, 40);
-    // naver_id_login.setDomain("http://localhost:5500/login");
-    // naver_id_login.setState(state);
-    // naver_id_login.setPopup();
-    // naver_id_login.init_naver_id_login();
-  }, []);
+  const NAVER_CLIENT_ID = "J71XESavo8zEzLQDSu3x";
+  const STATE = "flase";
+  const REDIRECT_URI = "http://localhost:5500/";
+  const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${STATE}&redirect_uri=${REDIRECT_URI}`;
+
+  const NaverLogin = () => {
+    window.location.href = NAVER_AUTH_URL;
+  };
 
   return (
     <Wrapper>
       <Contents>
         <Container>
-          {/* <LoginBtn>
+          <LoginBtn onClick={NaverLogin}>
             <SiNaver style={{ marginRight: "10px" }} />
             네이버로 로그인하기
-          </LoginBtn> */}
-          <div id="naver_id_login"></div>
+          </LoginBtn>
         </Container>
       </Contents>
     </Wrapper>
